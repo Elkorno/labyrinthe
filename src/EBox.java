12207b01a4602d3820257b2d00a7a2c3814375b5
Wiame El Korno <wiamewiwi@hotmail.com>
2002-01-06 00:47:29 +0100

    Méthode writeCharTo rajoutée

--- src/EBox.java
+++ src/EBox.java
@@ -1,8 +1,14 @@
+import java.io.PrintWriter;
+
 
 public class EBox extends MBox {
 	
 	public EBox(Maze maze, int line, int column)
 	{
 		super(maze,line,column);
+	}
+	public final void writeCharTo(PrintWriter pw)
+	{
+		pw.print('E');
 	}
 }
