e87f394c9b1015b9a6ed01a61d1ad307c3bcad73
Wiame El Korno <wiamewiwi@hotmail.com>
2002-01-05 21:15:31 +0100

    classe mère

---
+++ src/MBox.java
@@ -0,0 +1,30 @@
+public abstract class MBox
+implements VertexInterface
+{
+	private final Maze maze;
+	private final int line;
+	private final int column;
+		
+	public MBox(Maze maze,int line, int column)
+		{this.maze=maze;
+		 this.line=line;
+		 this.column=column;
+		}
+	public final String getLabel()
+	{ 
+		return "("+"line"+","+"column"+")";
+	}
+	public final int getColumn()
+	{
+	return column;
+	}
+	public final int getLine()
+	{
+		return line;
+	}
+	public boolean isAccessible()
+	{ 
+		return true;
+	}
+	
+}
