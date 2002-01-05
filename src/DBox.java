0a313ff5436a2b778f3168f479777cfecc138938
Wiame El Korno <wiamewiwi@hotmail.com>
2002-01-06 00:46:54 +0100

    Méthode writeCharTo rajoutée

--- src/DBox.java
+++ src/DBox.java
@@ -1,7 +1,13 @@
+import java.io.PrintWriter;
+
 public class DBox extends MBox {
 	
 	public DBox(Maze maze, int line, int column)
 	{ 
 		super(maze,line,column);
+	}
+	public final void writeCharTo(PrintWriter pw)
+	{
+		pw.print('D');
 	}
 }
