84f8e35846fbf64cbf47c0ab487ea2779390e2c5
Wiame El Korno <wiamewiwi@hotmail.com>
2002-01-05 20:23:46 +0100

    Je complète l'interface PiInterface

--- src/PiInterface.java
+++ src/PiInterface.java
@@ -1,4 +1,12 @@
 
 public interface PiInterface {
+	
+	/** donne la valeur de Pi */
+	
+public int getValue(VertexInterface vertex);
+
+/** on fixe le résultat de Pi pour un sommet donné */
+
+public void setValue(VertexInterface vertex, int value);
 
 }
