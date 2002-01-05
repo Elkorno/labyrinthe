f40acd365b37243c4ec4a504f3530cda449219cc
Wiame El Korno <wiamewiwi@hotmail.com>
2002-01-05 20:24:08 +0100

    Je complète l'interface PreviousInterface

--- src/PreviousInterface.java
+++ src/PreviousInterface.java
@@ -1,4 +1,17 @@
 
+import java.util.ArrayList;
+
+
 public interface PreviousInterface {
 
+	/** fixe la valeur pour un sommet*/
+	
+	public void setValue (VertexInterface vertex, VertexInterface value);
+	
+	/** donne la valeur pour un sommet*/
+	
+	public VertexInterface getValue(VertexInterface vertex);
+	
+	public ArrayList<VertexInterface> getShortestPathTo(VertexInterface vertex);
+	
 }
