022d88eb16e36758d1d7a7f0c7bd8e7b0e9565e7
Wiame El Korno <wiamewiwi@hotmail.com>
2002-01-05 20:23:21 +0100

    Je complète l'interface GraphInterface

--- src/GraphInterface.java
+++ src/GraphInterface.java
@@ -1,4 +1,21 @@
 
+import java.util.ArrayList;
+
+
 public interface GraphInterface {
+	
+	/** renvoie la liste des sommets */
+	
+public ArrayList<VertexInterface> getAllVertices();
+
+/** renvoie les sommets successeurs d'un sommet donné */
+
+public ArrayList<VertexInterface> getSuccessors(VertexInterface vertex);
+
+    /** renvoie le poids d'un arc */
+
+public int getWeight(VertexInterface src,VertexInterface dst);
+
+
 
 }
