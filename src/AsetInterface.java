62a166a4e9c270a69886e5f38084b949ec3e3ad6
Wiame El Korno <wiamewiwi@hotmail.com>
2002-01-05 20:22:43 +0100

    Je complète l'interface AsetInterface

--- src/AsetInterface.java
+++ src/AsetInterface.java
@@ -1,4 +1,12 @@
 
 public interface AsetInterface {
+	
+	/**ajouter un sommet à A */ 
+	public void add(VertexInterface vertex);
+	
+	/** tester l'appartenance d'un sommet à A */
+	
+	public boolean contains(VertexInterface vertex);
 
 }
+
