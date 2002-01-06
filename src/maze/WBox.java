7e16efd7423a5fa627165850f3bc6c3f0274200b
Wiame El Korno <wiamewiwi@hotmail.com>
2002-01-06 03:01:55 +0100

    Chgt

---
+++ src/maze/WBox.java
@@ -0,0 +1,21 @@
+package maze;
+import java.io.PrintWriter;
+
+
+public class WBox extends MBox {
+
+		public WBox(Maze maze, int line, int column)
+		
+		{ 
+			super(maze,line,column);
+		}
+@Override		
+public final boolean isAccessible()
+{ 
+	return false;
+}
+public final void writeCharTo(PrintWriter pw)
+{
+	pw.print('W');
+}
+}
