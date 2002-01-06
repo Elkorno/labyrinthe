5e021a6d4c850fa813965385e73e9251b312f9fa
Wiame El Korno <wiamewiwi@hotmail.com>
2002-01-06 03:00:23 +0100

    Chgt

---
+++ src/dijkstra/PiInterface.java
@@ -0,0 +1,14 @@
+package dijkstra;
+
+
+public interface PiInterface {
+	
+	/** donne la valeur de Pi */
+	
+public int getValue(VertexInterface vertex);
+
+/** on fixe le résultat de Pi pour un sommet donné */
+
+public void setValue(VertexInterface vertex, int value);
+
+}
