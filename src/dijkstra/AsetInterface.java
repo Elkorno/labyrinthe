9796783be9c473729758d09d78a33dc391101f3f
Wiame El Korno <wiamewiwi@hotmail.com>
2002-01-06 01:55:19 +0100

    Changement de position AsetInterface

---
+++ src/dijkstra/AsetInterface.java
@@ -0,0 +1,14 @@
+package dijkstra;
+
+
+public interface AsetInterface {
+	
+	/**ajouter un sommet à A */ 
+	public void add(VertexInterface vertex);
+	
+	/** tester l'appartenance d'un sommet à A */
+	
+	public boolean contains(VertexInterface vertex);
+
+}
+
